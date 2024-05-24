5c80fd319fd362f69790930d0b407d2310847437
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2024-05-24 15:53:52 +0700

    add final code

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// vite.config.js
+import glsl from 'vite-plugin-glsl'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	plugins: [glsl()],
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+	},
+})
